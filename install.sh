565c6a135c30331019c9655bc37a996b724ca304
Carey McLelland <carey.mclelland@cbc.ca>
2015-03-16 13:38:08 -0400

    deleted:    iTerm2/LiquidCarbon.itermcolors deleted:    iTerm2/LiquidCarbonTransparent.itermcolors deleted:    iTerm2/LiquidCarbonTransparentInverse.itermcolors modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,53 +1,30 @@
 #!/bin/bash
-version="0.9"
+#--
+# install script for github.com/mclellac dotfiles
+# Usage:
+#    sh <(curl -s https://raw.githubusercontent.com/mclellac/dotfiles/master/install.sh -L)
+#--
+dotconf="${HOME}/.config"
+dotdir="${dotconf}/dotfiles"
 declare -a dir=(
-    ${HOME}/.vim
-    ${HOME}/.vim/bundle
-    ${HOME}/.vim/autoload
-    ${HOME}/.vim/colors
-    ${HOME}/.vim/backup
+    ${HOME}/.config/vim
+    ${HOME}/.config/vim/bundle
+    ${HOME}/.config/vim/autoload
+    ${HOME}/.config/vim/colors
+    ${HOME}/.config/vim/backup
 )
-declare -a deps=(vim hg)
-package_list="/tmp/missing-packages.txt"
-# colours
+declare -a deps=(vim git hg)
+
+# text colour variables & output helper functions.
 green='\033[00;32m'
 red='\033[01;31m'
 white='\033[00;00m'
-cyan='\033[1;36m'
+errquit()    { msgwarn $err; exit 1; }
+msgsuccess() { msginfo $msg; }
+msginfo()    { message=${@:-"${white}Error: No message passed"}; printf "${green}${message}${white}\n"; }
+msgwarn()    { message=${@:-"${white}Error: No message passed"}; printf "${red}${message}${white}\n";   }
 
 cmd_exists() { [ -x "$(command -v "$1")" ] && printf 0 || printf 1; }
-
-usage() {
-cat << EOF
-Usage: ${0##*/}     [-h] 
-                    [-v] 
-                    [-i] <option> 
-
-Valid <option> for install: [vim|tmux|zsh|iTerm2]
-Example: 
-  To install the dotfiles for just vim type:
-    
-    ${0##*/} -i vim  
-
-No options will make an assumption that you want to setup everything
-in dotfiles.
-
-  Options
-  =======
-  -i <option>      * Install the files for vim, tmux, zsh or iTerm2.
-  -h               * Show this message.
-  -v               * Print version.
-EOF
-}
-
-
-mkdr() {
-    if [ ! -d $directory ]; then 
-        mkdir -p $directory >/dev/null 2>&1 && \
-            printf "${green}Directory: ${cyan}${directory} ${white}created.\n" || \
-            printf "${red}Error: ${white}Failed to create ${red}${directory} ${white}directory.\n"
-    fi
-}
 
 os_check() {
     os=`uname -s`
@@ -77,8 +54,9 @@ os_check() {
 
 dependency_check() {
     if [ -d ${HOME}/.vim/bundle/ ]; then
-        printf "${red}Moving old vim configuration files into ${HOME}/.vim.old${white}\n"
-        mv ${HOME}/.vim ${HOME}/.vim.`(date +%H%M-%d%m%y)` && mv ${HOME}/.vimrc ${HOME}/.vim.old
+        printf "${red}Moving old vim configuration files into ${HOME}/.vim.`(date +%H%M-%d%m%y)`${white}\n"
+        mkdir ${HOME}/.vim.`(date +%H%M-%d%m%y)` && 
+        mv ${HOME}/.vim ${HOME}/.vim.`(date +%H%M-%d%m%y)` && mv ${HOME}/.vimrc ${HOME}/.vim.`(date +%H%M-%d%m%y)`
     fi
 
     printf "Checking to see if the following applications have been installed:\n"
@@ -108,80 +86,26 @@ install_dependencies() {
         fi
     done
 
-    # delete /tmp/missing-packages.txt when done.
+    #-- delete /tmp/missing-packages.txt when done. --
     rm $package_list    
     vim_setup
 }
 
-copy_files() {
- echo 'copy_files()'
-}
-
-tmux_setup() {
-    cp tmux/tmux.conf ${HOME}/.tmux.conf
-}
-
-zsh_setup() {
-    for file in `(ls ./zsh)`; do cp $file ~/.${file}; done;
-}
-
-vim_setup() {
-    for directory in ${dir[@]}; do
-        mkdr $directory
+symlink() {
+    for file in `(find ${dotdir} -mindepth 2 -maxdepth 2 -type f -not -path '*/\.*')`; do 
+        ln $file `(echo $file | awk -F/ '{print $4}')`
     done
-
-    if [ -d ${HOME}/.vim/bundle ] && [ ! -d ${HOME}/.vim/bundle/vim-go ]; then
-        printf "${green}GitHub: ${cyan}fatih/vim-go.git  ${white}to ${cyan}${HOME}/.vim/bundle/vim-go. "
-        git clone https://github.com/fatih/vim-go.git ${HOME}/.vim/bundle/vim-go -q 
-        printf "${green}[done]${white}\n"
-    fi
-    
-    if [ -d ${HOME}/.vim/autoload ]; then
-        printf "${green}GitHub: ${cyan}gmarik/vundle.git ${white}to ${cyan}${HOME}/.vim/bundle/vundle. "
-        git clone https://github.com/gmarik/vundle.git ${HOME}/.vim/bundle/vundle -q 
-        printf "${green}[done]${white}\n"
-        sleep 1
-    fi
-
-    printf "${white}Installing plugins.\n"
-    sleep 1
-    cp vim/vimrc ${HOME}/.vimrc && vim +PluginInstall +qall
-    printf "${cyan}vimrc ${white}has been moved to ${cyan}~/.vimrc.\n"
-    printf "${green}Install complete.${white}\n"
 }
 
-main() {
-    install_app=""
+#-- check to make sure ~/.conf directory exists --
+[ -d ${dotconf} ] && echo "using ${dotconf}" || mkdir ${dotconf}; fi
 
-    while getopts "i:hv" options; do
-        echo $options
-        case "${options}" in
-            h|\?)
-                usage
-                exit 0
-                ;;
-            v)
-                printf "${version}\n"
-                exit 0
-                ;;
-            i)
-                install_app=${OPTARG}
-                ((  
-                    install_app == "vim"   || 
-                    install_app == "tmux"  ||
-                    install_app == "zsh"   ||
-                    install_app == "iTerm2"
-                )) || usage
-                ;;
-        esac
-    done
-    shift $((OPTIND-1))
+#-- clone or pull project from git --
+if [ ! -d $dotconf/dotfiles ]; then
+    git clone https://github.com/mclellac/dotfiles/ $dotconf && cd $dotconf
+elif [ -d $dotconf/dotfiles ]; then
+    cd $dotconf/dotfiles && git pull  && cd $dotconf
+fi
 
-    if [ -z "${s}" ] || [ -z "${p}" ]; then
-        usage
-    fi
-
-#    os_check
-}
-main
-#os_check
+os_check
+symlink
