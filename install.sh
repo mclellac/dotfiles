38df9718da87a80c7bb5844a34223a60745121e5
Carey McLelland <mclellac@ninsei.local>
2015-03-30 11:38:32 -0400

    updates

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ NORMAL=$(tput sgr0)
 WHITE=$(tput setaf 7)
 RED=$(tput setaf 1)
 
-separator()  { printf '%.0s-' {1..73}; echo }
+separator()  { printf '%.0s-' {1..73}; echo; }
 cmd_exists() { [ -x "$(command -v "$1")" ] && printf 0 || printf 1; }
 
 check_deps() {
