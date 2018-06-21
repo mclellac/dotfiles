d0729d9f6912e9266e3846f6214d2d86b09a40cf
Carey McLelland <carey.mclelland@cbc.ca>
2018-06-21 11:21:46 -0400

    Updates

--- polybar/launch-polybar.sh
+++ polybar/launch-polybar.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+
 if type "xrandr"; then
+    
+    m="$(xrandr | grep "connected primary" | awk '{print $1}')"
 
     for w in $( xrandr --query | grep "*" | awk -Fx '{print $1}' | tr -d ' '); do
-        WIDTH=${w} polybar --reload main &
+        MONITOR=${m} WIDTH=${w} polybar --reload main &
     done  
     
     #for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
