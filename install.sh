76ac71c0747012008109b675d44b94f93be1527a
Carey McLelland <carey.mclelland@cbc.ca>
2019-01-28 14:44:44 -0500

    Fix install.sh and remove old cruft.

--- install.sh
+++ install.sh
@@ -8,23 +8,20 @@ dotcfg="${HOME}/.config"
 dotdir="${dotcfg}/dotfiles"
 pkg_list="/tmp/missing-pkgs.txt"
 # Global colour variables
-grey="$(tput bold ; tput setaf 0)"
 red=$(tput setaf 1)
 green=$(tput setaf 2)
 cyan=$(tput setaf 6)
 white=$(tput setaf 7)
 yellow=$(tput setaf 11)
-blue=$(tput setaf 68)
-brown=$(tput setaf 130)
 orange=$(tput setaf 172)
 rst=$(tput sgr0)
 
 declare -a DIRECTORIES=(
-    ${HOME}/.vim
-    ${HOME}/.vim/bundle
-    ${HOME}/.vim/autoload
-    ${HOME}/.vim/colors
-    ${HOME}/.vim/backup
+    "${HOME}"/.vim
+    "${HOME}"/.vim/bundle
+    "${HOME}"/.vim/autoload
+    "${HOME}"/.vim/colors
+    "${HOME}"/.vim/backup
 )
 
 declare -a DEPS=(
@@ -41,52 +38,59 @@ declare -a DEPS=(
 )
 LEN=${#DEPS[*]}
 
-error_quit()    { message_error $err; exit 1; }
-message_ok()    { message=${@:-"[✘]: No OK content"};     printf "${green}[✔] ${message}${rst}\n"; }
-message_error() { message=${@:-"[✘]: No error content"};  printf "${red}[✘] ${message}${rst}\n";   }
+# error_quit()    { message_error $err; exit 1; }
+# message_ok()    { message=${@:-"[✘]: No OK content"};     printf "${green}[✔] ${message}${rst}\n"; }
+# message_error() { message=${@:-"[✘]: No error content"};  printf "${red}[✘] ${message}${rst}\n";   }
 cmd_exists()    { [ -x "$(command -v "$1")" ] && printf 0 || printf 1; }
 
 check_deps() {
     msg_box "Checking to see if the following applications have been installed"
 
-    for (( i=0; i<=(($LEN -1)); i++)); do
-        if [ $(cmd_exists ${DEPS[$i]}) -eq 0 ]; then
-            printf "${green}[✔]${rst} ${DEPS[$i]}\n"
+    for (( i=0; i<=((LEN -1)); i++ )); do
+        if [ "$(cmd_exists "${DEPS[$i]}")" -eq 0 ]; then
+            printf "%s[✔]%s %s\n" "${green}" "${rst}" "${DEPS[$i]}"
         else
-            printf "${red}[✘] ${DEPS[$i]}${rst} is missing.\n"
-            echo ${DEPS[$i]} >> $pkg_list
+            printf "%s[✘] %s%s is missing.\n" "${red}" "${DEPS[$i]}" "${rst}"
+            echo "${DEPS[$i]}" >> "${pkg_list}"
         fi
     done
 
-    # If pkg list exists; then install. Otherwise symlink files.
-    [ -f $pkg_list ] && install_deps || symlink_dotfiles
+    # If pkg_list exists; then install packages in list. Otherwise symlink files.
+    if [ -f "${pkg_list}" ]; then
+        install_deps
+    else
+        symlink_dotfiles
+    fi
 }
 
 pkg_mgr() {
     if [[ $OSTYPE == 'darwin'* ]]; then
+        app_regex='(weechat|mutt|polybar|i3|img|git|zsh)'
         app_installer="brew install"
-        [ ! -f ${HOME}/.zsh.osx ] && touch ${HOME}/.zsh.osx
+        [ ! -f "${HOME}"/.zsh.osx ] && touch "${HOME}"/.zsh.osx
     elif [[ $OSTYPE == 'freebsd'* ]]; then
+        app_regex='(mutt|polybar|i3|img|git|zsh)'
         bsd_installer="cd /usr/ports/devel/"
-        [ ! -f ${HOME}/.zsh.bsd ] && touch ${HOME}/.zsh.bsd
+        [ ! -f "${HOME}"/.zsh.bsd ] && touch "${HOME}"/.zsh.bsd
     elif [[ $OSTYPE == 'linux-gnu' ]]; then
-        [ ! -f ${HOME}/.zsh.gnu ] && touch ${HOME}/.zsh.gnu
-        elif [ $(cmd_exists pacman) ]; then
+        app_regex='(mutt|polybar|i3|img|git|zsh)'
+        [ ! -f "${HOME}"/.zsh.gnu ] && touch "${HOME}"/.zsh.gnu
+        elif [ "$(cmd_exists pacman)" ]; then
             app_installer="sudo pacman -Syuu"
-        if [ $(cmd_exists apt-get) ]; then
+        if [ "$(cmd_exists apt-get)" ]; then
             app_installer="sudo apt-get install"
-        elif [ $(cmd_exists dnf) ]; then
+        elif [ "$(cmd_exists dnf)" ]; then
             app_installer="sudo dnf install"
-        elif [ $(cmd_exists yum) ]; then
+        elif [ "$(cmd_exists yum)" ]; then
             app_installer="sudo yum install"
-        elif [ $(cmd_exists up2date) ]; then
+        elif [ "$(cmd_exists up2date)" ]; then
             app_installer="sudo up2date -i"
         else
-            printf "${red}[✘] ${white}No pkg manager found for this Linux system!\n"
+            printf "%s[✘] %sNo pkg manager found for this Linux system!\n" "${red}" "${white}"
             exit 2
         fi
     else
-        printf "${red}[✘] ${white}Unable to determine the operating system.\n"
+        printf "%s[✘] %sUnable to determine the operating system.\n" "${red}" "${white}"
         exit 2
     fi
 }
@@ -99,27 +103,27 @@ github_grab() {
 
     msg_box "Github: $user/$project"
 
-    if [ ! -d ${local_dir} ]; then
-        printf "Cloning: https://github.com/${cyan}${user}${rst}/${cyan}${project}${rst} to ${cyan}${local_dir}${rst}\n"
-        git clone --recursive https://github.com/${user}/${project} ${local_dir}
+    if [ ! -d "${local_dir}" ]; then
+        printf "Cloning: https://github.com/%s%s%s/%s%s%s to %s%s%s\n" "${cyan}" "${user}" "${rst}" "${cyan}" "${project}" "${rst}" "${cyan}" "${local_dir}" "${rst}"
+        git clone --recursive https://github.com/"${user}"/"${project}" "${local_dir}"
     else
-        printf "Updating: ${cyan}${project}${rst} in ${cyan}${local_dir}${rst}\n"
-        cd ${local_dir} && git pull
+        printf "Updating: %s%s%s in %s%s%s\n" "${cyan}" "${project}" "${rst}" "${cyan}" "${local_dir}" "${rst}"
+        cd "${local_dir}" && git pull
     fi
 }
 
 install_deps() {
     msg_box "Attempting to install missing pkgs."
-    for pkg in `(cat ${pkg_list})`; do
+    for pkg in $(cat "${pkg_list}"); do
         if [[ $OSTYPE != "freebsd"* ]]; then
-            $app_installer $pkg
+            $app_installer "${pkg}"
         else
-            $bsd_installer $pkg && make && sudo make install
+            $bsd_installer "${pkg}" && make && sudo make install
         fi
     done
 
     # Delete /tmp/missing-pkgs.txt when done
-    rm $pkg_list
+    rm "${pkg_list}"
 
     symlink_dotfiles
 }
@@ -127,10 +131,10 @@ install_deps() {
 make_dir() {
     local directories="$*"
 
-    if [ ! -d ${directories} ]; then
-        mkdir -p ${directories} >/dev/null 2>&1 && \
-            printf "${rst}DIRECTORIES: ${cyan}${directories} ${rst}created.\n" || \
-            printf "${red}Error: ${rst}Failed to create ${red}${directories} ${rst}directories.\n"
+    if [ ! -d "${directories}" ]; then
+        mkdir -p "${directories}" >/dev/null 2>&1 && \
+            printf "%sDIRECTORIES: %s%s%s created.\n" "${rst}" "${cyan}" "${directories}" "${rst}" || \
+            printf "%sError: %sFailed to create %s%s%s directories.\n" "${red}" "${rst}" "${red}" "${directories}" "${rst}"
     fi
 }
 
@@ -143,74 +147,79 @@ msg_box() {
     for line in "${str[@]}"; do
         ((msg_width<${#line})) && { msg_width="${#line}"; }
 
-        if [ $msg_width -gt $term_width ]; then
-            error_quit "error: msg_box() >> \$msg_width exceeds \$term_width. Split message into multiple lines or decrease the number of characters.\n"
+        if [ "${msg_width}" -gt "${term_width}" ]; then
+            error_quit "error: msg_box() >> \$msg_width exceeds \${term_width}. Split message into multiple lines or decrease the number of characters.\n"
         fi
 
-        x=$(($term_width - $msg_width))
-        pad=$(($x / 2))
+        x=$(("${term_width}" - "${msg_width}"))
+        pad=$(("${x}" / 2))
     done
 
     # draw box
-    printf '%s┌' "${orange}" && printf '%.0s─' {0..79} && printf '┐\n' && printf '│%79s │\n'
+    printf '%s┌' "${orange}" &&  printf '%.0s─' {0..79} && printf '┐\n' && printf '│%79s │\n' " "
 
     for line in "${str[@]}"; do
-        rpad=$((80 - $pad - $msg_width)) # make sure to close with width 80
-        printf "│%$pad.${pad}s" && printf '%s%*s' "$yellow" "-$msg_width" "$line" "${orange}" && printf "%$rpad.${rpad}s│\n"
+        rpad=$((80 - "${pad}" - "${msg_width}")) # make sure to close with width 80
+        printf "│%${pad}.${pad}s"
+        printf '%s%*s%s' "$yellow" "-$msg_width" "$line" "${orange}"
+        printf "%${rpad}.${rpad}s│\n"
     done
 
-    printf '│%79s │\n' && printf  '└' && printf '%.0s─' {0..79}  && printf '┘\n%s' ${rst}
+    printf '│%79s │\n' " " && printf  '└' && printf '%.0s─' {0..79}  && printf '┘\n%s' "${rst}"
 }
 
 vim_setup() {
-    for dir in ${DIRECTORIES[@]}; do
-        make_dir $dir
+    msg_box "Setting up Vim."
+    for dir in "${DIRECTORIES[@]}"; do
+        make_dir "${dir}"
     done
 
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-    printf "${rst}Installing vim plugins: ${cyan} vim +PlugInstall +qall${rst}\n"
+    printf "%sInstalling vim plugins: %svim +PlugInstall +qall%s\n" "${rst}" "${cyan}" "${rst}"
     sleep 1
     vim +PlugInstall +qall
 }
 
 symlink_dotfiles() {
-    msg_box "Symlinking files to ${HOME}"
+    msg_box "Symlinking files to: ${HOME}."
 
-    for file in `(find $dotdir -mindepth 2 -maxdepth 2 -type f -not -path '\(.*)' | grep -vE '(iterm2|jhbuild|i3|img|irssi|git|weechat|zsh)')`; do
+    for file in $(find "${dotdir}" -mindepth 2 -maxdepth 2 -type f -not -path '\(.*)' | grep -vE "${app_regex}"); do
         # soft_ln variable stores the absolute path for the symlink
-        soft_ln=${HOME}/.`(echo ${file} | awk -F/ '{print $7}')`
+        softln="${HOME}"/.$(echo "${file}" | awk -F/ '{print $7}')
 
-        if [ ! -f ${soft_ln} ]; then
-            ln -s ${file} ${soft_ln}
+        if [ ! -f "${softln}" ]; then
+            echo "ln -s ${file} ${softln}"
+            ln -s "${file}" "${softln}"
         else
-            rm ${soft_ln} && ln -s ${file} ${soft_ln}
+            echo "\$ rm ${softln} && ln -s ${file} ${softln}"
+            rm "\$ ${softln}" && ln -s "${file}" "${softln}"
         fi
     done
 
     # Copy git config files into $HOME, as we don't want them symlinked and mistakenly git pushed
-    for file in `(ls ${dotdir}/git)`; do
-        cp ${dotdir}/git/$file ${HOME}/.${file}
+    for file in $(ls "${dotdir}"/git); do
+        cp "${dotdir}"/git/"${file}" "${HOME}"/."${file}"
     done
 
     if [[ $OSTYPE == 'linux-gnu' ]]; then
-        if [ -d ${HOME}/.i3 ]; then
-            mv ${HOME}/.i3 ${HOME}/.i3.old && make_dir ${HOME}/.i3
+        if [ -d "${HOME}"/.i3 ]; then
+            mv "${HOME}"/.i3 "${HOME}"/.i3.old && make_dir "${HOME}"/.i3
         else
-            make_dir ${HOME}/.i3
+            make_dir "${HOME}"/.i3
         fi
 
-        ln -s ${dotdir}/i3/config ${HOME}/.i3/config
-        ln -s ${dotdir}/i3/i3blocks.conf ${HOME}/.i3/i3blocks.conf
-        cp -R ${dotdir}/i3/scripts ${HOME}/.i3/scripts
+        ln -s "${dotdir}"/i3/config "${HOME}"/.i3/config
+        ln -s "${dotdir}"/i3/i3blocks.conf "${HOME}"/.i3/i3blocks.conf
+        cp -R "${dotdir}"/i3/scripts "${HOME}"/.i3/scripts
     fi
 
     # Only symlink zshrc to ~. Keep the rest of the zsh config
     # files in ~/.config/dotfiles/zsh and source them from there.
-    ln -s ${dotdir}/zsh/zshrc ${HOME}/.zshrc
+    ln -s "${dotdir}"/zsh/zshrc "${HOME}"/.zshrc
 
     # Is zplug installed? Install if it isn't.
-    if [ ! -d ${HOME}/.zplug ]; then
+    if [ ! -d "${HOME}"/.zplug ]; then
         msg_box "Installing zplug"
         curl -sL --proto-redir -all,https \
             https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
@@ -224,21 +233,21 @@ if [[ -d "${dotcfg}" ]]; then
    msg_box "Using: ${dotcfg}"
 else
    make_dir "${dotcfg}"
+fi
 
 # Check for ~/.zprivate file, create default if doesn't exist.
 [ ! -f "${HOME}"/.zprivate ] && printf "# private variables\nexport WORK_DOMAIN=\"\"\nexport email=\"\"\nexport work_email=\"\"\n" >> .zprivate
 
 # Clone or pull project from git
-github_grab $dotcfg/dotfiles mclellac dotfiles
+github_grab "${dotcfg}"/dotfiles mclellac dotfiles
 
 pkg_mgr
 check_deps
 
 # Install nerd-fonts for terminal emulator
-msg_box "Installing nerd-fonts from:" \
-        "https://github.com/ryanoasis/nerd-fonts"
-github_grab ${dotcfg}/nerd-fonts ryanoasis nerd-fonts && sh ${dotcfg}/nerd-fonts/install.sh
+# msg_box "Installing nerd-fonts from:" \
+#         "https://github.com/ryanoasis/nerd-fonts"
+# github_grab "${dotcfg}"/nerd-fonts ryanoasis nerd-fonts && sh "${dotcfg}"/nerd-fonts/install.sh
 
 msg_box "Installation complete."
-printf "** Set your name and email in: ${cyan}${HOME}/.gitconfig${rst} **\n"
-
+printf "** Set your name and email in: %s%s/.gitconfig%s **\n" "${cyan}" "${HOME}" "${rst}"
