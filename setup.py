ce1711569242253baf1be78254fa7e1de459f00e
Carey McLelland <careymclelland@gmail.com>
2023-08-16 11:15:14 -0400

    Removed BASH post install actions and converted them to python

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 """
-██████╗  ██████╗ ████████╗███████╗
-██╔══██╗██╔═══██╗╚══██╔══╝██╔════╝
-██║  ██║██║   ██║   ██║   ███████╗
-██║  ██║██║   ██║   ██║   ╚════██║
-██████╔╝╚██████╔╝   ██║   ███████║
-╚═════╝  ╚═════╝    ╚═╝   ╚══════╝
+                        ██████╗  ██████╗ ████████╗███████╗
+                        ██╔══██╗██╔═══██╗╚══██╔══╝██╔════╝
+                        ██║  ██║██║   ██║   ██║   ███████╗
+                        ██║  ██║██║   ██║   ██║   ╚════██║
+                        ██████╔╝╚██████╔╝   ██║   ███████║
+                        ╚═════╝  ╚═════╝    ╚═╝   ╚══════╝
 """
 print(__doc__)
 
@@ -17,9 +17,10 @@ import argparse
 import os
 import platform
 import subprocess
-import unicodedata
 import logging
 import yaml
+
+from os import makedirs
 
 
 # Color formatting functions
@@ -41,70 +42,151 @@ def setup_logging():
     logging.basicConfig(format="%(message)s", stream=sys.stdout, level=logging.INFO)
 
 
-def log(msg, cr=True):
+def log(msg, color=None, cr=True, action_title=None, errors=None):
+    """
+    Log a message with optional color, action title, error handling, and newline control.
+
+    Args:
+        msg (str): The message to log.
+        color (callable, optional): A color formatting function to apply to the message.
+                                    For example: color_wrap("\033[0;31m") to make the message red.
+        cr (bool, optional): Whether to add a newline after logging the message. Default is True.
+        action_title (str, optional): Title of the action associated with the message.
+        errors (list, optional): A list to append errors or messages along with their action titles.
+
+    Examples:
+        log("Regular log message")
+        log("Colored log message", color=RED)
+        log("Error message", color=RED, action_title="My Action")
+        log("Error message with continue", color=RED, action_title="My Action", errors=[])
+
+    """
+    if color:
+        msg = color(msg)
+
+    if action_title:
+        msg = f"{action_title}: {msg}"
+
     logging.info(msg)
+
+    if errors is not None and action_title is not None:
+        errors.append((action_title, msg))
+
     if cr:
         logging.info("")
 
 
-def log_with_color(msg, color=WHITE, cr=True):
-    log(color(msg), cr=cr)
+def message_box(msg, color=WHITE, use_bold=False, len_adjust=0):
+    # Adjust the width to 80 characters
+    box_width = 80
 
+    # Split the message into multiple lines if it's too long
+    lines = []
+    words = msg.split()
+    current_line = ""
+    for word in words:
+        if len(current_line) + len(word) + 1 <= 50:  # Maximum line length is 50 characters
+            current_line += " " + word
+        else:
+            lines.append(current_line)
+            current_line = word
+    if current_line:
+        lines.append(current_line)
 
-def log_error(message, action_title=None):
-    log_with_color(f"Error in {action_title}: {message}", color=RED)
+    # Calculate padding for centering the message
+    pad_width = (box_width - max(len(line) for line in lines)) // 2
 
+    # Construct the message box
+    box_lines = [
+        "┏" + ("━" * box_width) + "┓",
+        *["┃" + " " * pad_width + line.ljust(box_width - pad_width * 2) + " " * pad_width + "┃" for line in lines],
+        "┗" + ("━" * box_width) + "┛",
+    ]
 
-def log_error_and_continue(message, action_title=None, errors=None):
-    log_error(message, action_title)
-    if errors is not None:
-        errors.append((action_title, message))
-
-
-def log_boxed(msg, color=WHITE, use_bold=False, len_adjust=0):
-    pad_msg = " " + msg + "  "
-    l = sum(not unicodedata.combining(ch) for ch in pad_msg) + len_adjust
+    # Join the box lines and log the message
+    box_msg = "\n".join(box_lines)
     if use_bold:
-        log_with_color(
-            "┏" + ("━" * l) + "┓\n" + "┃" + pad_msg + "┃\n" + "┗" + ("━" * l) + "┛\n", color=color, cr=False
-        )
+        log(box_msg, color=color, cr=False)
     else:
-        log_with_color(
-            "┌" + ("─" * l) + "┐\n" + "│" + pad_msg + "│\n" + "└" + ("─" * l) + "┘\n", color=color, cr=False
-        )
+        log(box_msg, color=color, cr=False)
+
+
+def create_symbolic_link(target, source, args):
+    # bad entry if source does not exist...
+    if not os.path.lexists(source):
+        log(RED("source %s : does not exist" % source))
+        return
+
+    # if --force option is given, delete and override the previous symlink
+    if os.path.lexists(target):
+        is_broken_link = os.path.islink(target) and not os.path.exists(os.readlink(target))
+
+        if args.force or is_broken_link:
+            if os.path.islink(target):
+                os.unlink(target)
+            else:
+                log(
+                    "{:60s} : {}".format(
+                        BLUE(target), YELLOW("already exists but not a symlink; --force option ignored")
+                    ),
+                    cr=False,
+                )
+        else:
+            log(
+                "{:60s} : {}".format(
+                    BLUE(target),
+                    GRAY("already exists, skipped")
+                    if os.path.islink(target)
+                    else YELLOW("exists, but not a symbolic link. Check by yourself!!"),
+                ),
+                cr=False,
+            )
+
+    # make a symbolic link if available
+    if not os.path.lexists(target):
+        mkdir_target = os.path.split(target)[0]
+        makedirs(mkdir_target, exist_ok=True)
+        log(GREEN("Created directory : %s" % mkdir_target))
+        os.symlink(source, target)
+        log("{:20s} {} {}".format(BLUE(target), CYAN("━━"), GREEN("'%s'" % source)), cr=False)
 
 
 def execute_action(action, errors):
     try:
         subprocess.run(action, shell=True)
     except Exception as e:
-        log_error_and_continue(str(e), "execute_action", errors)
+        log(str(e), "execute_action", errors)
 
 
-def execute_post_action(action, action_title, args, errors):
-    try:
-        if callable(action):
-            action(args)  # Call the function if it's a regular function
-        elif isinstance(action, str):
-            execute_action(action, errors)  # Execute the bash command
-    except Exception as e:
-        log_error(str(e), action_title)
-        errors.append(action_title)
+def execute_post_install_actions(actions, args, errors):
+    for action in actions:
+        try:
+            if callable(action):
+                if action.__name__ == "action_gitconfig_secret":
+                    action()  # Call the function without any arguments
+                else:
+                    action(args, errors)  # Pass both args and errors to the function
+            elif isinstance(action, list):
+                execute_action(action, errors)  # Execute the bash command
+        except Exception as e:
+            log("An error occurred while executing a post install action:", color=RED)
+            log(str(e))
+            errors.append(action)
 
 
 def load_config(filename):
     try:
         with open(filename, "r") as file:
             config = yaml.safe_load(file)
-            log_boxed("YAML config opened.", color=CYAN)
+            message_box("YAML config opened.", color=YELLOW)
         return config
     except Exception as e:
-        log_error(f"Error loading config file: {e}")
+        log(f"Error loading config file: {e}")
         sys.exit(1)
 
 
 def action_zgen_update(args, errors):
-    log_boxed("Action: zgen update", color=CYAN)
+    message_box("Action: zgen update", color=CYAN)
 
     # Source zplug and list plugins
     zsh_command = """
@@ -136,47 +218,50 @@ def action_vim_update(vim_executable, args, errors):
             is_neovim = True
 
         if is_neovim:
-            log_boxed('Action: Neovim update', color=CYAN)
-            log_with_color('nvim --headless "+Lazy! sync" +qa && echo "nvim sync\'d" || echo "nvim sync failed"', color=WHITE)
-            vim_command = f'{vim_executable} --headless "+Lazy! sync" +qa && echo "Sync complete." || echo "Neovim sync failed."'
+            message_box("Action: Neovim update", color=CYAN)
+            log('nvim --headless "+Lazy! sync" +qa && echo "nvim sync\'d" || echo "nvim sync failed"', color=WHITE)
+            vim_command = (
+                f'{vim_executable} --headless "+Lazy! sync" +qa && echo "Sync complete." || echo "Neovim sync failed."'
+            )
         else:
-            log_boxed("Action: Vim update", color=CYAN)
-            log_with_color("vim +PlugUpdate +qall", color=WHITE)
-            vim_command = f'{vim_executable} +PlugUpdate +qall'
+            message_box("Action: Vim update", color=CYAN)
+            log("vim +PlugUpdate +qall", color=WHITE)
+            vim_command = f"{vim_executable} +PlugUpdate +qall"
 
         if not args.skip_vimplug:
             subprocess.run(vim_command, shell=True)
         else:
-            log_with_color(f'{vim_command} (Skipped)', colorfn=CYAN)
+            log(f"{vim_command} (Skipped)", colorfn=CYAN)
     except Exception as e:
-        log_error_and_continue(str(e), "action_vim_update", errors)
+        log(str(e), "action_vim_update", errors)
+
 
 def action_install_neovim_py(args, errors):
-    log_boxed("Action: neovim", color=CYAN)
+    message_box("Action: neovim", color=CYAN)
 
     try:
         subprocess.run(["bash", "install-neovim-py.sh"])
     except Exception as e:
-        log_error_and_continue(str(e), "action_install_neovim_py", errors)
+        log(str(e), "action_install_neovim_py", errors)
 
 
 def action_shell_to_zsh(args, errors):
-    log_boxed("Action: Change shell to zsh", color=CYAN)
+    message_box("Action: Change shell to zsh", color=CYAN)
 
     if not shutil.which("/bin/zsh"):
-        log_error("Error: /bin/zsh not found. Please install zsh.", "action_shell_to_zsh", errors)
+        log("Error: /bin/zsh not found. Please install zsh.", "action_shell_to_zsh", errors)
         sys.exit(1)
 
     current_shell = os.path.basename(os.environ["SHELL"])
     if current_shell.lower() != "zsh":
-        log_with_color("Please type your password if you wish to change the default shell to ZSH", color=YELLOW)
+        log("Please type your password if you wish to change the default shell to ZSH", color=YELLOW)
         subprocess.run(["chsh", "-s", "/bin/zsh"])
     else:
-        log_with_color("Users shell is already zsh.", color=WHITE)
+        log("Users shell is already zsh.", color=WHITE)
 
 
 def action_gitconfig_secret(errors):
-    log_boxed("~/gitconfig.secret", color=CYAN)
+    message_box("Action: gitconfig.secret", color=CYAN)
 
     gitconfig_secret_path = os.path.expanduser("~/.gitconfig.secret")
 
@@ -196,7 +281,7 @@ def action_gitconfig_secret(errors):
         user_email = config_result.stdout.strip() if config_result.returncode == 0 else None
 
         if not user_name or not user_email:
-            log_with_color("[!!!] Please configure git user name and email:", color=YELLOW)
+            log("[!!!] Please configure git user name and email:", color=YELLOW)
             if not user_name:
                 git_username = input("(git config user.name) Please input your name  : ")
             else:
@@ -211,15 +296,15 @@ def action_gitconfig_secret(errors):
                 subprocess.run(["git", "config", "--file", gitconfig_secret_path, "user.name", git_username])
                 subprocess.run(["git", "config", "--file", gitconfig_secret_path, "user.email", git_useremail])
             else:
-                log_error("Missing name or email, exiting.", color=RED)
+                log("Missing name or email, exiting.", color=RED)
                 sys.exit(1)
         else:
-            log_with_color("Git user name and email are already set with the values:", color=WHITE)
-            log_with_color("user.name  : " + user_name, color=GREEN)
-            log_with_color("user.email : " + user_email, color=GREEN)
+            log("Git user name and email are already set with the values:", color=WHITE)
+            log("user.name  : " + user_name, color=GREEN, cr=False)
+            log("user.email : " + user_email, color=GREEN, cr=True)
 
     except Exception as e:
-        log_error_and_continue(str(e), "action_gitconfig_secret", errors)
+        log(str(e), "action_gitconfig_secret", errors)
 
 
 def main():
@@ -242,7 +327,7 @@ def main():
     current_dir = os.path.abspath(os.path.dirname(__file__))
     os.chdir(current_dir)
 
-    log_boxed("Setting symbolic links from config.yaml", color=CYAN)
+    message_box("Setting symbolic links from config.yaml", color=CYAN)
     for task in tasks:
         os_condition = task.get("os")
         if os_condition and os_condition != current_os:
@@ -251,87 +336,36 @@ def main():
         target = os.path.expanduser(task.get("target", ""))
         source = os.path.join(current_dir, os.path.expanduser(task.get("source", "")))
 
-        # bad entry if source does not exists...
-        if not os.path.lexists(source):
-            log(RED("source %s : does not exist" % source))
-            continue
-
-        # if --force option is given, delete and override the previous symlink
-        if os.path.lexists(target):
-            is_broken_link = os.path.islink(target) and not os.path.exists(os.readlink(target))
-
-            if args.force or is_broken_link:
-                if os.path.islink(target):
-                    os.unlink(target)
-                else:
-                    log(
-                        "{:50s} : {}".format(
-                            BLUE(target), YELLOW("already exists but not a symbolic link; --force option ignored")
-                        )
-                    )
-            else:
-                log(
-                    "{:50s} : {}".format(
-                        BLUE(target),
-                        GRAY("already exists, skipped")
-                        if os.path.islink(target)
-                        else YELLOW("exists, but not a symbolic link. Check by yourself!!"),
-                    )
-                )
-
-        # make a symbolic link if available
-        if not os.path.lexists(target):
-            mkdir_target = os.path.split(target)[0]
-            makedirs(mkdir_target, exist_ok=True)
-            log(GREEN("Created directory : %s" % mkdir_target))
-            os.symlink(source, target)
-            log("{:50s} : {}".format(BLUE(target), GREEN("symlink created from '%s'" % source)))
+        create_symbolic_link(target, source, args)
 
     errors = []  # Initialize an empty list to collect errors
 
-    # Define a list of executable names
-    vim_executables = ["nvim", "vim"]
+    message_box("Post Install Actions", color=YELLOW, use_bold=True)
+    # Post install actions
+    post_install_actions = [
+        action_install_neovim_py,
+        action_shell_to_zsh,
+        lambda _, errors: action_gitconfig_secret(errors),  # Pass errors to action_gitconfig_secret
+        action_zgen_update,
+        ["bash", "install-tmux.sh"],
+    ]
 
-    # Iterate through the list and execute action_vim_update for each executable
+    execute_post_install_actions(post_install_actions, args, errors)
+
+    # Setup Vim & Neovim
+    vim_executables = ["nvim", "vim"]  # Iterate through the list and execute action_vim_update for each executable
+
     for executable in vim_executables:
         action_vim_update(executable, args, errors)
 
-    log_boxed("                    [ Post Actions ]                    ", color=YELLOW, use_bold=True)
-    post_actions = [
-        (lambda: action_install_neovim_py(args, errors)),
-        (lambda: action_shell_to_zsh(args, errors)),
-        (lambda: action_gitconfig_secret(errors)),
-        (lambda: action_zgen_update(args, errors)),
-        (lambda vim_executable, args, errors: action_vim_update(vim_executable, args, errors))(
-            "nvim" if shutil.which("nvim") else "vim", args, errors
-        ),
-        (["bash", "install-tmux.sh"], errors),  # Using a custom message for this action
-    ]
-
-    for action in post_actions:
-        try:
-            if callable(action):
-                action()  # Call the function if it's a regular function
-            elif isinstance(action, list):
-                execute_action(action, errors)  # Execute the bash command
-            # Add this section to iterate through vim_executables
-            elif callable(action[0]) and isinstance(action[1], list) and "vim" in action[1]:
-                vim_executables = ["nvim", "vim"]
-                for executable in vim_executables:
-                    action[0](executable, args, errors)
-        except Exception as e:
-            log_with_color("An error occurred while executing a post install action:", color=RED)
-            log(str(e))
-            errors.append(action)
-
     if errors:
         print("Inside the errors block")
-        log_boxed("You have %3d warnings or errors -- check the logs!" % len(errors), color=YELLOW, use_bold=True)
+        message_box("You have %3d warnings or errors -- check the logs!" % len(errors), color=YELLOW, use_bold=True)
         for action_title, error_message in errors:
-            log_with_color(f"   [{action_title}] {error_message}", color=RED)
+            log(f"   [{action_title}] {error_message}", color=RED)
         log("\n")
     else:
-        log_boxed("✔  You are all set! ", color=GREEN, use_bold=True)
+        message_box("✔  You are all set! ", color=GREEN, use_bold=True)
 
     log("- Please restart the shell (e.g. " + CYAN("`exec zsh`") + ") if necessary.")
     log("\n\n", cr=False)
