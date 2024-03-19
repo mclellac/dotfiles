0682bcd345407a2d475a97f54b9429cc93384283
Carey McLelland <careymclelland@gmail.com>
2024-03-19 12:40:05 -0400

    bump.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,9 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
 -- set leader key to space
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
 
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>")
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
