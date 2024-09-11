7ded74406b8b3be69e26c1dd1953550114231533
Carey McLelland <careymclelland@gmail.com>
2024-09-11 11:55:18 -0400

    lvim updates

--- lvim/config.lua
+++ lvim/config.lua
@@ -2,20 +2,17 @@
 
 -- Reload user-defined configurations
 reload "user.plugins"
-reload "user.options"
+reload "user.autocommands"
 reload "user.nvimtree"
 reload "user.null-ls"
 reload "user.treesitter"
 reload "user.telescope"
+reload "user.harpoon"
+reload "user.neotest"
+reload "user.options"
 
 -- Install and setup Mason
 -- require("mason").setup()
 
 -- Automatically install LSP servers, formatters, linters, and DAP servers
 -- require("user.mason")
-
--- Add `pyright` to `skipped_servers` list as we will use ruff/ruff_ls
---vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
-
--- Ensure system tools are installed
--- require("user.tools")
