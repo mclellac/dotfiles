b5d3b5890ec850a4b7689c201def836424303619
Carey McLelland <careymclelland@gmail.com>
2025-03-25 10:45:26 -0400

    nvim optiond & keymaps.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local keymap = vim.keymap
+
+local opts = { noremap = true, silent = true }
+
 -- Paste the clipboard register
 vim.keymap.set("n", "<leader>p", '"0p', { desc = "Paste after cursor" })
 vim.keymap.set("x", "<leader>p", '"0p', { desc = "Paste after cursor" })
@@ -26,3 +30,24 @@ vim.api.nvim_set_keymap("x", "<A-j>", "<Nop>", { noremap = true, silent = true }
 vim.api.nvim_set_keymap("x", "<A-k>", "<Nop>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("x", "J", "<Nop>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("x", "K", "<Nop>", { noremap = true, silent = true })
+
+-- DIRECTORY NAVIGATION ------------------------------------------------------
+keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", opts)
+keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", opts)
+keymap.set("n", "<C-k>", "<C-w>k", opts) -- NAVIGATE [^] UP
+keymap.set("n", "<C-h>", "<C-w>h", opts) -- NAVIGATE [<] LEFT
+keymap.set("n", "<C-l>", "<C-w>l", opts) -- NAVIGATE [>] RIGHT
+keymap.set("n", "<C-j>", "<C-w>j", opts) -- NAVIGATE [v] DOWN
+
+-- WINDOW MANAGEMENT ---------------------------------------------------------
+keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- SPLIT VERTICALLY
+keymap.set("n", "<leader>sh", ":split<CR>", opts) -- SPLIT HORIZONTALLY
+
+-- INDENT --------------------------------------------------------------------
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+
+-- COMMENTS ------------------------------------------------------------------
+vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
+vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
+
