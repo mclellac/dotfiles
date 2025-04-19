4d3e314e050b6d75415248577594832186dbd8d7
Carey McLelland <careymclelland@gmail.com>
2025-04-19 10:58:02 -0400

    hypr updates & walls.

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,13 +2,13 @@
 require("config.lazy")
 
 -- 80 char width lines are ridiculous
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "python",
-  callback = function()
-    vim.opt_local.textwidth = 100
-    vim.opt_local.colorcolumn = "100"
-  end,
-})
+--vim.api.nvim_create_autocmd("FileType", {
+--  pattern = "python",
+--  callback = function()
+--    vim.opt_local.textwidth = 100
+--    vim.opt_local.colorcolumn = "100"
+--  end,
+--})
 
 -- Settings for both Nvim-qt and Neovide
 local font_name = "JetBrainsMono Nerd Font"
