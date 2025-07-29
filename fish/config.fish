6069a43a3e2acee57b0748da2ff5a04eec8f51bc
Carey McLelland <carey.mclelland@cbc.ca>
2025-07-29 14:47:28 -0400

    bump

--- fish/config.fish
+++ fish/config.fish
@@ -1,168 +1,29 @@
-# Fish shell configuration
-
-# Set environment variables
-set -x STARSHIP_CONFIG ~/.config/starship/custom.toml
-set -x LANG "en_US.UTF-8"
-set -x TERM "xterm-256color"
-set -x GO111MODULE on
-set -x GOPATH "$HOME/Projects"
-set -x GOROOT "/usr/local/opt/go/libexec"
-set -x DENO_INSTALL "$HOME/.deno"
-
-# Set paths
-set -x PATH "/usr/local/opt/llvm/bin" "$HOME/.config/emacs/bin" "$HOME/.local/bin" "/usr/local/opt/yq@3/bin" "$PATH"
-set -x PATH "$GOPATH/bin" "$GOROOT/bin" "$PATH"
-set -x PATH "$DENO_INSTALL/bin" "$PATH"
-if test -d /usr/local/opt/curl/bin
-    set -x PATH /usr/local/opt/curl/bin $PATH
-end
-if test -d "$HOME/Library/Python/3.10/bin"
-    set -x PATH "$HOME/Library/Python/3.10/bin" "$PATH"
-end
-if test -d "$HOME/.cargo/bin"
-    set -x PATH "$HOME/.cargo/bin" "$PATH"
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 end
 
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
 
-# Aliases
-alias ls 'ls -G'
-alias vim 'nvim'
-alias msfconsole 'msfconsole -x "db_connect postgres@msf"'
-alias diff 'colordiff'
-alias python 'python3'
-alias ll 'ls -alG'
-alias gitlog "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-alias gitstatus 'git status -uno'
-alias history 'history 0'
-alias agenda 'gcalcli agenda'
-alias calw 'clear; gcalcli calw -w 12'
-alias calm 'clear; gcalcli calm -w 20'
-alias hd 'od -Ax -tx1 -v'
-alias ccat 'ccat -G String="brown" -G Keyword="darkblue" -G Comment="lightgrey" -G Type="teal" -G Literal="teal" -G Punctuation="darkred" -G Plaintext="darkblue" -G Tag="blue" -G HTMLTag="lightgreen" -G HTMLAttrName="blue" -G HTMLAttrValue="green" -G Decimal="darkblue"'
-alias mutt 'neomutt'
-alias grep 'grep --color'
-alias podls 'echo "Pods running in current context, and the container images inside them" && kubectl get pods --all-namespaces -o=jsonpath=\'{range .items[*]}{"\\n"}{.metadata.name}{":\\t"}{range .spec.containers[*]}{.image}{", "}{end}{end}\' | grep -E -v \'(fluent|heap|l7|kube)\' | sed \'s/;$//\' | sort'
-alias nmap-scripts-dir 'cd /usr/share/nmap/scripts'
-alias ose_services 'oc get services --all-namespaces --template \'{{range .items}}{{if ne .spec.clusterIP "None"}}{{.metadata.name}} {{.metadata.namespace}} {{.spec.clusterIP}} {{(index .spec.ports 0).protocol}} {{(index .spec.ports 0).port}}{{"\\n"}}{{end}}{{end}}\' | sed  -e \'s/^/ ⎈ /g\' -e \'s/ TCP / tcp → /g\' -e \'s/ UDP / udp → /g\''
-alias ose_pods 'oc get pods --all-namespaces --template \'{{range .items}}{{if .status.containerStatuses}}{{if (index .status.containerStatuses 0).ready}}{{if not .spec.hostNetwork}}{{.spec.nodeName}} {{.status.hostIP}} {{.metadata.name}} {{.metadata.namespace}} {{.status.podIP}} {{printf "%.21s" (index .status.containerStatuses 0).containerID}}{{"\\n"}}{{end}}{{end}}{{end}}{{end}}\' | sed -e \'s|docker://||\''
-alias cputop 'top -bc -d 60 -n 5'
-if command -v lvim >/dev/null 2>&1
-    alias vim lvim
-else if command -v nvim >/dev/null 2>&1
-    alias vim nvim
 end
 
-# Functions
-function digans
-    dig "$argv" +noall +answer
-end
-
-function digsoa
-    dig "$argv" SOA +noall +answer
-end
-
-function digmx
-    dig "$argv" MX +noall +answer
-end
-
-function digns
-    dig "$argv" NS +noall +answer
-end
-
-function digany
-    dig "$argv" ANY +noall +answer
-end
-
-function kubepass
-    kubectl config view -o jsonpath='{.users[?(@.name == "'"$argv"'")].user.password}'
-end
-
-function kubectx
-    kubectl config current-context
-end
-
-function kubeip
-    kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="ExternalIP")].address}'
-end
-
-function showcert
-    echo "openssl s_client -showcerts -connect $argv"
-    echo -n | openssl s_client -showcerts -connect $argv
-end
-
-function ssldump
-    set NIC $argv[1]
-    set PORT $argv[2]
-    sudo tcpdump -ni $NIC "tcp port $PORT and (tcp[((tcp[12] & 0xf0) >> 2)] = 0x16)" -vvvv
-end
-
-function check_compression
-    curl -s -I -H 'Accept-Encoding: br,gzip,deflate' $argv | grep -i "Content-Encoding"
-end
-
-function cmdhist
-    history | awk '{print $2}' | sort | uniq -c | sort -rn | head -20 | sed 's/.\///g' | awk '!max{max=$1;}{r=""; i=s=60 * $1/max; while(i-->0) r=r"█"; printf "\033[1;34m %15s \033[0m %4d \033[1;36m %s \033[0m %s", $2, $1, r, "\n";}'
-end
-
-function connerize
-    echo "connery: $argv" | sed -e "s/s/sh/g" -e "s/shsh/sh/g" -e "s/shh/sh/g"
-end
-
-function connerize-say
-    echo "$argv" | sed -e "s/s/sh/g" -e "s/shsh/sh/g" -e "s/shh/sh/g" | say -v "Alex" -i -r 200
-end
-
-function akcurl
-    set -l headers "Pragma: akamai-x-get-request-id,akamai-x-get-cache-key,akamai-x-cache-on,akamai-x-cache-remote-on,akamai-x-get-true-cache-key,akamai-x-check-cacheable,akamai-x-get-extracted-values,akamai-x-feo-trace,x-akamai-logging-mode: verbose"
-    echo "curl -sIXGET \"$argv\" -H \"$headers\""
-    curl -sIXGET "$argv" -H "$headers"
-end
-
-function cache-key-curl
-    set -l headers "Pragma: akamai-x-get-cache-key,akamai-x-get-true-cache-key,akamai-x-check-cacheable"
-    echo "curl -sIXGET \"$argv\" -H \"$headers\""
-    curl -sIXGET "$argv" -H "$headers"
-end
-
-function akhttp
-  set -l headers '"Pragma: akamai-x-get-request-id,akamai-x-get-cache-key,akamai-x-cache-on,akamai-x-cache-remote-on,akamai-x-get-true-cache-key,akamai-x-check-cacheable,akamai-x-get-extracted-values,akamai-x-feo-trace,x-akamai-logging-mode: verbose"'
-  echo "http --headers \"$argv\" $headers"
-  http --headers "$argv" $headers
-end
-
-function aklab
-    set -l AKFLAGS "-H \"Pragma: akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-get-request-id\" -H \"x-akamai-logging-mode: verbose\" -vv -H \"User-Agent: AU-KSD\""
-    curl -I $AKFLAGS "$argv"
-end
-
-function calc
-    set -l result (printf "scale=10;%s\n" "$argv" | bc --mathlib | tr -d '\\\n')
-    if string match -q ".*\\..*" $result
-        printf "%s" $result | sed -e 's/^\./0./' -e 's/^-\./-0./' -e 's/0*$//;s/\.$//'
-    else
-        printf "%s" $result
-    end
-    printf "\n"
-end
-
-
-# FZF
-set -x FZF_DEFAULT_OPTS "
-	--color=fg:#797593,bg:#faf4ed,hl:#d7827e
-	--color=fg+:#575279,bg+:#f2e9e1,hl+:#d7827e
-	--color=border:#dfdad9,header:#286983,gutter:#faf4ed
-	--color=spinner:#ea9d34,info:#56949f
-	--color=pointer:#907aa9,marker:#b4637a,prompt:#797593"
-
-# Starship
 starship init fish | source
-
-# iTerm2 Shell Integration
-if test -e "$HOME/.iterm2_shell_integration.fish"
-   source "$HOME/.iterm2_shell_integration.fish"
+if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
 end
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc' ];
-    source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc';
-end
+alias vim nvim
+alias pamcan pacman
+alias ls 'eza --icons'
+alias clear "printf '\033[2J\033[3J\033[1;1H'"
+alias q 'qs -c ii'
+
+# function fish_prompt
+#   set_color cyan; echo (pwd)
+#   set_color green; echo '> '
+# end
--- quickshell/ii/services/Ai.qml
+++ quickshell/ii/services/Ai.qml
@@ -6,6 +6,7 @@ import qs.modules.common
 import qs
 import Quickshell
 import Quickshell.Io
+import Quickshell.Wayland
 import QtQuick
 import "./ai/"
 
@@ -25,7 +26,15 @@ Singleton {
     readonly property string interfaceRole: "interface"
     readonly property string apiKeyEnvVarName: "API_KEY"
 
-    property string systemPrompt: Config.options?.ai?.systemPrompt ?? ""
+    property string systemPrompt: {
+        let prompt = Config.options?.ai?.systemPrompt ?? "";
+        for (let key in root.promptSubstitutions) {
+            // prompt = prompt.replaceAll(key, root.promptSubstitutions[key]);
+            // QML/JS doesn't support replaceAll, so use split/join
+            prompt = prompt.split(key).join(root.promptSubstitutions[key]);
+        }
+        return prompt;
+    }
     // property var messages: []
     property var messageIDs: []
     property var messageByID: ({})
@@ -52,13 +61,20 @@ Singleton {
     }
 
     function safeModelName(modelName) {
-        return modelName.replace(/:/g, "_").replace(/\./g, "_")
+        return modelName.replace(/:/g, "_").replace(/\./g, "_").replace(/ /g, "-").replace(/\//g, "-")
     }
 
     property list<var> defaultPrompts: []
     property list<var> userPrompts: []
     property list<var> promptFiles: [...defaultPrompts, ...userPrompts]
     property list<var> savedChats: []
+
+    property var promptSubstitutions: {
+        "{DISTRO}": SystemInfo.distroName,
+        "{DATETIME}": `${DateTime.time}, ${DateTime.collapsedCalendarFormat}`,
+        "{WINDOWCLASS}": ToplevelManager.activeToplevel.appId,
+        "{DE}": `${SystemInfo.desktopEnvironment} (${SystemInfo.windowingSystem})` 
+    }
 
     // Gemini: https://ai.google.dev/gemini-api/docs/function-calling
     // OpenAI: https://platform.openai.com/docs/guides/function-calling
@@ -190,6 +206,19 @@ Singleton {
             "key_get_description": Translation.tr("**Pricing**: free. Data used for training.\n\n**Instructions**: Log into Google account, allow AI Studio to create Google Cloud project or whatever it asks, go back and click Get API key"),
             "api_format": "gemini",
         }),
+        "gemini-2.5-flash-pro": aiModelComponent.createObject(this, {
+            "name": "Gemini 2.5 Pro",
+            "icon": "google-gemini-symbolic",
+            "description": Translation.tr("Online | Google's model\nGoogle's state-of-the-art multipurpose model that excels at coding and complex reasoning tasks."),
+            "homepage": "https://aistudio.google.com",
+            "endpoint": "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro:streamGenerateContent",
+            "model": "gemini-2.5-pro",
+            "requires_key": true,
+            "key_id": "gemini",
+            "key_get_link": "https://aistudio.google.com/app/apikey",
+            "key_get_description": Translation.tr("**Pricing**: free. Data used for training.\n\n**Instructions**: Log into Google account, allow AI Studio to create Google Cloud project or whatever it asks, go back and click Get API key"),
+            "api_format": "gemini",
+        }),
         "gemini-2.5-flash-lite": aiModelComponent.createObject(this, {
             "name": "Gemini 2.5 Flash-Lite",
             "icon": "google-gemini-symbolic",
@@ -225,6 +254,17 @@ Singleton {
     }
     property ApiStrategy currentApiStrategy: apiStrategies[models[currentModelId]?.api_format || "openai"]
 
+    Connections {
+        target: Config
+        function onReadyChanged() {
+            if (!Config.ready) return;
+            (Config?.options.ai?.extraModels ?? []).forEach(model => {
+                const safeModelName = root.safeModelName(model["model"]);
+                root.addModel(safeModelName, model)
+            });
+        }
+    }
+
     Component.onCompleted: {
         setModel(currentModelId, false, false); // Do necessary setup for model
     }
@@ -250,6 +290,10 @@ Singleton {
         return result;
     }
 
+    function addModel(modelName, data) {
+        root.models[modelName] = aiModelComponent.createObject(this, data);
+    }
+
     Process {
         id: getOllamaModels
         running: true
@@ -262,7 +306,7 @@ Singleton {
                     root.modelList = [...root.modelList, ...dataJson];
                     dataJson.forEach(model => {
                         const safeModelName = root.safeModelName(model);
-                        root.models[safeModelName] = aiModelComponent.createObject(this, {
+                        root.addModel(safeModelName, {
                             "name": guessModelName(model),
                             "icon": guessModelLogo(model),
                             "description": Translation.tr("Local Ollama model | %1").arg(model),
