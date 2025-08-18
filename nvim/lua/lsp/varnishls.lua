0e8b12c8df3fa4666ebf2c6a13e2bd2c6eabae70
Carey McLelland <careymclelland@gmail.com>
2025-08-18 13:37:12 -0400

    bump.

--- nvim/lua/lsp/varnishls.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- Point to varnishls (add --debug for debug log)
-  cmd = { "varnishls", "lsp", "--stdio" },
-  filetypes = { "vcl", "vtc" },
-  root_markers = { ".varnishls.toml", ".git" },
-  settings = {},
-}
